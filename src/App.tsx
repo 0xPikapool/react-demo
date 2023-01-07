5b47f63be460ccb4742eefb76e49e892f4dcd046
braixen <pikapool-braixen@protonmail.com>
2023-01-07 18:11:26 +0400

    feat: basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
-
 import { Account } from "./components";
 import { BidForm } from "./components/BidForm";
 
 export function App() {
   const { isConnected } = useAccount();
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "10vh",
+      }}
+    >
       <h1>Pikapool React Demo</h1>
 
       <ConnectButton />
       {isConnected && <BidForm />}
-    </>
+    </div>
   );
 }
--- src/components/BidForm.tsx
+++ src/components/BidForm.tsx
@@ -21,54 +21,64 @@ export function BidForm() {
 
   return (
     <>
-      <div>
-        <h3>Auction Configuration</h3>
-        <label htmlFor="auctionName">Auction Name</label>
-        <input
-          id="auctionName"
-          type="text"
-          value={auctionName}
-          onChange={(e) => {
-            setAuctionName(e.target.value);
-          }}
-        />
-        <br />
-        <label htmlFor="auctionAddress">Auction Address</label>
-        <input
-          id="auctionAddress"
-          type="text"
-          value={auctionAddress}
-          onChange={(e) => {
-            e.target.value.startsWith("0x") &&
-              setAuctionAddress(e.target.value as `0x${string}`);
-          }}
-        />
-        <br />
-        <label htmlFor="basePrice">Base Price</label>
-        <input
-          id="basePrice"
-          type="text"
-          value={basePrice}
-          onChange={(e) => setBasePrice(Number(e.target.value))}
-        />
-        <h3>Bid Configuration</h3>
-        <label htmlFor="amount">Amount</label>
-        <input
-          id="amount"
-          type="number"
-          value={amount}
-          onChange={(e) => setAmount(Number(e.target.value))}
-        />
-        <br />
-        <label htmlFor="tip">Tip</label>
-        <input
-          id="tip"
-          type="number"
-          value={tip}
-          onChange={(e) => setTip(Number(e.target.value))}
-        />
-        <br />
-        <button onClick={() => signAndSubmit()}>Sign</button>
+      <div style={{ display: "flex", gap: "3rem", marginTop: "2rem" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h3>Auction Configuration</h3>
+          <label htmlFor="auctionName">Name</label>
+          <input
+            id="auctionName"
+            type="text"
+            value={auctionName}
+            onChange={(e) => {
+              setAuctionName(e.target.value);
+            }}
+          />
+          <br />
+          <label htmlFor="auctionAddress">Address</label>
+          <input
+            id="auctionAddress"
+            type="text"
+            value={auctionAddress}
+            onChange={(e) => {
+              e.target.value.startsWith("0x") &&
+                setAuctionAddress(e.target.value as `0x${string}`);
+            }}
+          />
+          <br />
+          <label htmlFor="basePrice">Base Price</label>
+          <input
+            id="basePrice"
+            type="text"
+            value={basePrice}
+            onChange={(e) => setBasePrice(Number(e.target.value))}
+          />
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h3>Bid Configuration</h3>
+          <label htmlFor="amount">Amount</label>
+          <input
+            id="amount"
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+          />
+          <br />
+          <label htmlFor="tip">Tip</label>
+          <input
+            id="tip"
+            type="number"
+            value={tip}
+            onChange={(e) => setTip(Number(e.target.value))}
+          />
+          <br />
+          <br />
+          <button
+            style={{ alignSelf: "bottom" }}
+            onClick={() => signAndSubmit()}
+          >
+            Charge Meta-Transaction ⚡
+          </button>
+        </div>
       </div>
       <br />
       {error && <div>Error: {error.message}</div>}

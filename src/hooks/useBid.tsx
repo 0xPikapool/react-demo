cec655ea02ee24f47cb1898e8f80a94f1b1c988c
ash-keccakum <119022199+ash-keccakum@users.noreply.github.com>
2023-01-07 22:44:24 -0500

    temporarily change back to 16

--- src/hooks/useBid.tsx
+++ src/hooks/useBid.tsx
@@ -48,7 +48,7 @@ export default function useBid(
     domain: {
       name: "Pikapool",
       version: "v0",
-      chainId: "0x" + chainId.toString(5),
+      chainId: "0x" + chainId.toString(16),
       verifyingContract: pikapoolOptions.settlementContract,
     },
 
